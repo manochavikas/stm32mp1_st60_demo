1f8b7d2dedfb030d99e050397e5b33dd94e5b484
Vikas Manocha <vikas.manocha@st.com>
2020-04-10 14:10:43 -0700

    fix extra newline from gst command
    Signed-off-by: Vikas Manocha <vikas.manocha@st.com>

--- scripts/tx_script.sh
+++ scripts/tx_script.sh
@@ -1,2 +1,1 @@
-gst-launch-1.0 filesrc location=/usr/local/demo/media/ST2297_visionv3.webm ! matroskademux !
-rtpvp8pay ! udpsink host=192.168.1.100 port=5100
+gst-launch-1.0 filesrc location=/usr/local/demo/media/ST2297_visionv3.webm ! matroskademux ! rtpvp8pay ! udpsink host=192.168.1.100 port=5100
