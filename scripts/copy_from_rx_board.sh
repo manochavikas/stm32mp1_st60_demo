55b6036d9625b147b0746f7bfa4bde75b4595982
Vikas Manocha <vikas.manocha@st.com>
2020-04-14 09:35:55 -0700

    copy from tx board to rx board to avoid confusion with naming
    Signed-off-by: Vikas Manocha <vikas.manocha@st.com>

--- scripts/copy_from_rx_board.sh
+++
@@ -1,5 +0,0 @@
-while true;
-do
-	date
-	scp 192.168.1.100:/usr/local/demo/media/ST2297_visionv3.webm .
-done;
---
+++ scripts/copy_from_tx_board.sh
@@ -0,0 +1,5 @@
+while true;
+do
+	date
+	scp 192.168.1.200:/usr/local/demo/media/ST2297_visionv3.webm .
+done;
