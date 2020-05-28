e47fa544aaab9a4000a587df4fe82870bb3e3ede
Vikas Manocha <vikas.manocha@st.com>
2020-05-27 19:28:02 -0700

    replaced watch with script with break: watch was not working correctly with demo_launcher
    Signed-off-by: Vikas Manocha <vikas.manocha@st.com>

--- add_in_demo_launcher/eth_streaming/bin/tx_eth_stream.sh
+++ add_in_demo_launcher/eth_streaming/bin/tx_eth_stream.sh
@@ -1,1 +1,4 @@
-watch gst-launch-1.0 filesrc location=/usr/local/demo/media/ST2297_visionv3.webm ! matroskademux ! rtpvp8pay ! udpsink host=192.168.1.255 port=5100
+while true
+do
+gst-launch-1.0 filesrc location=/usr/local/demo/media/ST2297_visionv3.webm ! matroskademux ! rtpvp8pay ! udpsink host=192.168.1.255 port=5100 || break
+done
